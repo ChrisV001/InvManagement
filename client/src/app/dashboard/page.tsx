b54af3a88fa55831d8b85a9febee792cb8922c34
ChrisV001 <kvasilovski@yahoo.com>
2024-10-01 18:58:20 +0200

    Commit 3, dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,40 @@
+"use client";
+
+
+import CardPopularProducts from "@/app/dashboard/CardPopularProducts";
+import CardPurchaseSummary from "@/app/dashboard/CardPurchaseSummary";
+import CardSalesSummary from "@/app/dashboard/cardSalesSummary"
+import CardExpenseSummary from "@/app/dashboard/CardExpenseSummary";
+import {Package, TrendingUp} from "lucide-react";
+import StatCard from "@/app/dashboard/StatCard";
+
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+            <CardPopularProducts/>
+            <CardSalesSummary/>
+            <CardPurchaseSummary/>
+            <CardExpenseSummary/>
+            <StatCard title="Customer & Expenses"
+                      primaryIcon={<Package className="text-blue-600 w-6 h-6"/>}
+                      dateRange="22 - 29 October 2023"
+                      details={[
+                          {
+                              title: "Customer Growth", amount: "175.00",
+                              changePercentage: 131, IconComponent: TrendingUp
+                          },
+                          {
+                              title: "Expenses", amount: "10.00",
+                              changePercentage: -56, IconComponent: TrendingUp
+                          },
+
+                      ]}/>
+            <div className="row-span-3 xl:row-span-6 bg-gray-500"/>
+            <div className="row-span-3 xl:row-span-6 bg-gray-500"/>
+            <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"/>
+            <div className="row-span-3 bg-gray-500"/>
+
+        </div>
     )
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,20 +4,26 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-/* route imports */
-
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* configs */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* route imports */
+app.use("/dashboard", dashboardRoutes);
+
+//SERVER
+const port = process.env.port || 3001;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+
